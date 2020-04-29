e8d6161179e36fd71f91fecf42495d5e6a093c33
psk9211 <wdcup20002@gmail.com>
2020-04-29 20:54:07 +0900

    add word cloud

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def get_parser():
                         help='Target URL')
     # parser.add_argument('--keyword', type=str, default='None',
     #                     help='Keyword you want to find')
-    # parser.add_argument('--mode', type=str, default='crawl',
-    #                     help='crawl : get new crawl data\nfind : find the information using keyword')
+    parser.add_argument('--mode', type=str, default='cloud',
+                        help='crawl : get new crawl data\nfind : find the information using keyword')
     parser.add_argument('--dir', type=str, default='./data/',
                         help='Directory path for saving crawled data')
 
@@ -28,11 +28,20 @@ if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args(sys.argv[1:])
 
-    posters, spotlights, talks = crawl_main(args)
-    print('Saving crawled data...')
-    utils.save_crawled_data(args, papers=posters, name='poster_')
-    utils.save_crawled_data(args, papers=spotlights, name='spot_lights_')
-    utils.save_crawled_data(args, papers=talks, name='talk_')
-    print('Done.')
+    if args.mode == 'crawl':
+        posters, spotlights, talks = crawl_main(args)
+        print('Saving crawled data...')
+        utils.save_crawled_data(args, papers=posters, name='poster_')
+        utils.save_crawled_data(args, papers=spotlights, name='spot_lights_')
+        utils.save_crawled_data(args, papers=talks, name='talk_')
+        # print('Done.')
+        print('Crawling complete!')
 
-    print('Crawling complete!')
+    elif args.mode == 'cloud':
+        # print('Word Cloud Visualization')
+        utils.word_cloud(args)
+        # print('end')
+
+    else:
+        print('Invalid argument: ' + args.mode + ', exiting...')
+        exit()
--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import json
 import datetime
+import os
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+from wordcloud import WordCloud
+from PIL import Image
+from collections import Counter
+
 
 
 def save_crawled_data(args, papers, name):
@@ -8,3 +16,52 @@ def save_crawled_data(args, papers, name):
 
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(papers, f, indent='\t')
+
+
+def make_mask(icon_path):
+    icon = Image.open(icon_path).convert("RGBA")
+    mask = Image.new("RGB", icon.size, (255, 255, 255))
+    mask.paste(icon, icon)
+    mask = np.array(mask)
+
+    return mask
+
+
+def word_cloud(args):
+    file_regex = r'(.*)_((\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})-(\d{2})).json'
+    files_dir = os.listdir(args.dir)
+    files = []
+
+    for file in enumerate(files_dir):
+        files.append(file[1])
+
+    crawled_date = re.search(file_regex, files[0]).group(2)
+    keywords = []
+    total_paper = 0
+
+    for i in range(len(files)):
+        dict = json.loads(open(args.dir + files[i], 'r').read())
+
+        for j in range(len(dict)):
+            temp = [x.strip() for x in dict[j]["Keywords:"].split(',')]
+            keywords += temp
+            total_paper += 1
+
+    word_cloud_dict = Counter(keywords)
+    # plt_mask = np.array(Image.open("./mask.png"))
+    plt_mask = make_mask("./mask.png")
+
+    cloud = WordCloud(background_color='white', max_font_size=500, mask=plt_mask, max_words=1500, min_word_length=2)
+    cloud.generate_from_frequencies(word_cloud_dict)
+
+    top10 = word_cloud_dict.most_common(10)
+    print("Total accepted paper: " + str(total_paper))
+    print("\n*****Top-10 Keywords*****")
+    for i in range(10):
+        print(top10[i][0] + ': ' + str(top10[i][1]) + ' times')
+    print("*" * 25)
+
+    plt.figure(figsize=(20, 10))
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.axis('off')
+    cloud.to_file('./' + crawled_date + '_result.png')
